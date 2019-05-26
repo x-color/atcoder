c59ef6c546d76203d4deaaff32bda28efb50e8cd
x-color <36035885+x-color@users.noreply.github.com>
2019-05-26 13:07:11 +0900

    Add answer to ABC127C

---
+++ ABC127/C/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Split(bufio.ScanWords)
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
+	sc.Scan()
+	m, _ := strconv.Atoi(sc.Text())
+
+	ll := 0
+	rr := n
+	for i := 0; i < m; i++ {
+		sc.Scan()
+		l, _ := strconv.Atoi(sc.Text())
+		sc.Scan()
+		r, _ := strconv.Atoi(sc.Text())
+		if l > ll {
+			ll = l
+		}
+		if r < rr {
+			rr = r
+		}
+	}
+
+	fmt.Println(max(0, min(n-ll, rr-ll)+1))
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
