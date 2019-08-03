32649fb3defa3a0f1a5c7af6fdd882680080f839
x-color <36035885+x-color@users.noreply.github.com>
2019-08-03 12:48:40 +0900

    Add int slice scanner in library

--- library/input.go
+++ library/input.go
@@ -26,3 +26,15 @@ func scanStrings(s *bufio.Scanner, vals ...*string) {
 		*vals[i] = s.Text()
 	}
 }
+
+func scanNAndInts(s *bufio.Scanner) (int, []int) {
+	s.Scan()
+	n, _ := strconv.Atoi(s.Text())
+	vals := make([]int, n)
+	for i := 0; i < n; i++ {
+		s.Scan()
+		m, _ := strconv.Atoi(s.Text())
+		vals[i] = m
+	}
+	return n, vals
+}
