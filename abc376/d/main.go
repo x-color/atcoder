79bcbdc6cb3257523d42a2e6b10e5dee2e7dea76
x-color <36035885+x-color@users.noreply.github.com>
2024-11-03 22:03:43 +0900

    Add abc376/cd

---
+++ abc376/d/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	defer out.Flush()
+
+}
+
+// --- utils ---
+
+// --- utils ---
+// --- io ---
+var (
+	in  = bufio.NewScanner(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func init() {
+	in.Split(bufio.ScanWords)
+}
+
+func ReadInt() int {
+	in.Scan()
+	n, _ := strconv.Atoi(in.Text())
+	return n
+}
+
+func ReadInt2() (int, int) {
+	return ReadInt(), ReadInt()
+}
+
+func ReadInt3() (int, int, int) {
+	return ReadInt(), ReadInt(), ReadInt()
+}
+
+func ReadInts(n int) []int {
+	l := make([]int, n)
+	for i := 0; i < n; i++ {
+		l[i] = ReadInt()
+	}
+	return l
+}
+
+func ReadString() string {
+	in.Scan()
+	return in.Text()
+}
+
+func ReadStrings(n int) []string {
+	l := make([]string, n)
+	for i := 0; i < n; i++ {
+		l[i] = ReadString()
+	}
+	return l
+}
+
+func ReadStringInt() (string, int) {
+	return ReadString(), ReadInt()
+}
+
+func PrintSlice[T any](l []T) {
+	v := make([]any, len(l))
+	for i, n := range l {
+		v[i] = n
+	}
+	fmt.Fprintln(out, v...)
+}
+
+// --- math ---
+
+type Number interface {
+	int | int32 | int64 | float32 | float64
+}
+
+func Min[T Number](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max[T Number](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Abs[T Number](a T) T {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// --- board ---
+
+type Square[T any] struct {
+	value   T
+	visited bool
+}
+
+type Position struct {
+	x int
+	y int
+}
+
+type Board[T any] [][]Square[T]
+
+func NewBoard[T any](h, w int, f func(rune) T) Board[T] {
+	board := make(Board[T], h)
+	for i := 0; i < h; i++ {
+		s := ReadString()
+		board[i] = make([]Square[T], w)
+		for j := 0; j < w; j++ {
+			board[i][j] = Square[T]{
+				value: f(rune(s[j])),
+			}
+		}
+	}
+	return board
+}
+
+func (b Board[T]) Width() int {
+	return len(b[0])
+}
+
+func (b Board[T]) Height() int {
+	return len(b)
+}
+
+func (b Board[T]) In(p Position) bool {
+	return p.x < 0 || p.x >= b.Width() || p.y < 0 || p.y >= b.Height()
+}
+
+func (b Board[T]) At(p Position) Square[T] {
+	return b[p.y][p.x]
+}
+
+func (b Board[T]) Visit(p Position) {
+	b[p.y][p.x].visited = true
+}
+
+func (b Board[T]) Leave(p Position) {
+	b[p.y][p.x].visited = false
+}
+
+func (b Board[T]) IsVisited(p Position) bool {
+	return b[p.y][p.x].visited
+}
+
+func (b Board[T]) Neighbors4(pos Position) []Position {
+	return []Position{
+		{x: pos.x + 1, y: pos.y},
+		{x: pos.x - 1, y: pos.y},
+		{x: pos.x, y: pos.y + 1},
+		{x: pos.x, y: pos.y - 1},
+	}
+}
+
+func (b Board[T]) Neighbors8(pos Position) []Position {
+	return []Position{
+		{x: pos.x + 1, y: pos.y},
+		{x: pos.x - 1, y: pos.y},
+		{x: pos.x, y: pos.y + 1},
+		{x: pos.x, y: pos.y - 1},
+		{x: pos.x + 1, y: pos.y + 1},
+		{x: pos.x - 1, y: pos.y - 1},
+		{x: pos.x - 1, y: pos.y + 1},
+		{x: pos.x + 1, y: pos.y - 1},
+	}
+}
