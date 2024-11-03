c71ccfade08393df9dd1a5a8e6993de9333202ca
x-color <36035885+x-color@users.noreply.github.com>
2024-11-03 20:44:08 +0900

    Add utils for board

--- abc378/d/main.go
+++ abc378/d/main.go
@@ -11,47 +11,43 @@ func main() {
 	defer out.Flush()
 
 	h, w, k := ReadInt3()
-	board := make([][]bool, h)
-	for i := 0; i < h; i++ {
-		s := ReadString()
-		board[i] = make([]bool, w)
-		for j := 0; j < w; j++ {
-			board[i][j] = s[j] == '.'
-		}
-	}
+	board := NewBoard(h, w, func(r rune) bool {
+		return r == '#'
+	})
 
 	routes := 0
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
-			routes += dfs(board, x, y, k)
+			routes += dfs(board, Position{x, y}, k)
 		}
 	}
 
 	fmt.Fprintln(out, routes)
 }
 
-func dfs(board [][]bool, x, y, k int) int {
-	if x < 0 || x >= len(board[0]) || y < 0 || y >= len(board) {
+func dfs(board Board[bool], pos Position, k int) int {
+	if board.In(pos) {
 		return 0
 	}
-	if !board[y][x] {
+	if board.IsVisited(pos) {
+		return 0
+	}
+	if board.At(pos).value {
 		return 0
 	}
 	if k == 0 {
 		return 1
 	}
 
-	board[y][x] = false
+	board.Visit(pos)
 	routes := 0
-	routes += dfs(board, x+1, y, k-1)
-	routes += dfs(board, x-1, y, k-1)
-	routes += dfs(board, x, y+1, k-1)
-	routes += dfs(board, x, y-1, k-1)
-	board[y][x] = true
+	for _, p := range board.Neighbors4(pos) {
+		routes += dfs(board, p, k-1)
+	}
+	board.Leave(pos)
+
 	return routes
 }
-
-// --- utils ---
 
 // --- utils ---
 // --- io ---
@@ -136,4 +132,82 @@ func Abs[T Number](a T) T {
 		return a
 	}
 	return -a
+}
+
+// --- board ---
+
+type Square[T any] struct {
+	value   T
+	visited bool
+}
+
+type Position struct {
+	x int
+	y int
+}
+
+type Board[T any] [][]Square[T]
+
+func NewBoard[T any](h, w int, f func(rune) T) Board[T] {
+	board := make(Board[T], h)
+	for i := 0; i < h; i++ {
+		s := ReadString()
+		board[i] = make([]Square[T], w)
+		for j := 0; j < w; j++ {
+			board[i][j] = Square[T]{
+				value: f(rune(s[j])),
+			}
+		}
+	}
+	return board
+}
+
+func (b Board[T]) Width() int {
+	return len(b[0])
+}
+
+func (b Board[T]) Height() int {
+	return len(b)
+}
+
+func (b Board[T]) In(p Position) bool {
+	return p.x < 0 || p.x >= b.Width() || p.y < 0 || p.y >= b.Height()
+}
+
+func (b Board[T]) At(p Position) Square[T] {
+	return b[p.y][p.x]
+}
+
+func (b Board[T]) Visit(p Position) {
+	b[p.y][p.x].visited = true
+}
+
+func (b Board[T]) Leave(p Position) {
+	b[p.y][p.x].visited = false
+}
+
+func (b Board[T]) IsVisited(p Position) bool {
+	return b[p.y][p.x].visited
+}
+
+func (b Board[T]) Neighbors4(pos Position) []Position {
+	return []Position{
+		{x: pos.x + 1, y: pos.y},
+		{x: pos.x - 1, y: pos.y},
+		{x: pos.x, y: pos.y + 1},
+		{x: pos.x, y: pos.y - 1},
+	}
+}
+
+func (b Board[T]) Neighbors8(pos Position) []Position {
+	return []Position{
+		{x: pos.x + 1, y: pos.y},
+		{x: pos.x - 1, y: pos.y},
+		{x: pos.x, y: pos.y + 1},
+		{x: pos.x, y: pos.y - 1},
+		{x: pos.x + 1, y: pos.y + 1},
+		{x: pos.x - 1, y: pos.y - 1},
+		{x: pos.x - 1, y: pos.y + 1},
+		{x: pos.x + 1, y: pos.y - 1},
+	}
 }
